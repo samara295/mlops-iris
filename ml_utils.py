27dfa9080ed48fd09001c609818a84cb85e3d207
Samarasimha Reddy Panga <samara295@gmail.com>
2021-07-08 18:31:19 +0530

    Updated ml_utils.py with the decision tree classifier

--- ml_utils.py
+++ ml_utils.py
@@ -2,10 +2,12 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
 
 # define a Gaussain NB classifier
-clf = GaussianNB()
-
+#clf = GaussianNB()
+clf = DecisionTreeClassifier(random_state=0)
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
 r_classes = {y: x for x, y in classes.items()}
